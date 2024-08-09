96424f547d7bbb6ec2781498e6492a47a08ef764
quentin-mace <154054112+quentin-mace@users.noreply.github.com>
2024-08-09 17:12:52 +0200

    Added a link for monitoring

--- views/templates/main.php
+++ views/templates/main.php
@@ -25,7 +25,11 @@
             <?php 
                 // Si on est connecté, on affiche le bouton de déconnexion, sinon, on affiche le bouton de connexion : 
                 if (isset($_SESSION['user'])) {
+                    echo '<a href="index.php?action=admin">Admin</a>';
+                    echo '<a href="index.php?action=monitoring">Monitoring</a>';
                     echo '<a href="index.php?action=disconnectUser">Déconnexion</a>';
+                } else {
+                    echo '<a href="index.php?action=connectionForm">Connexion</a>';
                 }
                 ?>
         </nav>
@@ -37,7 +41,7 @@
     </main>
     
     <footer>
-        <p>Copyright © Emilie Forteroche 2023 - Openclassrooms - <a href="index.php?action=admin">Admin</a>
+        <p>Copyright © Emilie Forteroche 2023 - Openclassrooms
     </footer>
 
 </body>
